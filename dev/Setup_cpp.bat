bfa17b63e9a539470e448113f99c6bebec7ec566
CD <cdickson@aquaveo.com>
2018-09-27 16:03:03 -0600

    Manually compile for VS2013 64-bit.  Move manual files to dev.

--- dev/Setup_cpp.bat
+++ dev/Setup_cpp.bat
@@ -5,10 +5,11 @@ echo OPTIONALLY DELETE BUILD FOLDER BEFORE MAKING IT NEXT: build_cpp
 pause
 
 echo on
-mkdir ..\build_cpp
+mkdir build_cpp
 pushd .
-cd ..\build_cpp
-conan install -pr ..\dev\xmsprofile_debug .. --build missing
+cd build_cpp
+REM conan install -pr ..\xmsprofile_debug ..\.. --build missing
+conan install -pr ..\xmsprofile_debug ..\..
 popd
 echo off
 
@@ -18,16 +19,14 @@ pause
 
 echo on
 pushd .
-cd ..\build_cpp
-cmake .. -G"Visual Studio 14 2015 Win64" -DIS_PYTHON_BUILD=False -DBUILD_TESTING=True
+cd build_cpp
+cmake .. -G"Visual Studio 12 2013 Win64" -DIS_PYTHON_BUILD=False -DBUILD_TESTING=True
 popd
 echo off
 
 echo.
 echo ABORT IF ERRORS ABOVE
-pause
-
 echo.
-echo OPEN VS2015 FILE build_cpp\xms*.sln
+echo OPEN VS2015 FILE build_cpp\*.sln
 echo BUILD AND RUN PROJECT runner FOR DEBUG x64
 pause
